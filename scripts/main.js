246f72fe542550984204d432d2b1e25c7373064d
Nick Rupert <nickrupert7@gmail.com>
2018-07-26 23:44:07 -0400

    Added logic to transition between queues

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,16 @@
   var SELECTOR_CLOCK_WEEKDAY = '#main-header-date-time-day';
   var SELECTOR_CLOCK_DATE = '#main-header-date-time-date';
   var SELECTOR_CLOCK_TIME = '#main-header-date-time-time';
-  var SELECTOR_QUEUE = '#main-queue';
+  var SELECTOR_QUEUE_HEADER = '#main-queue-header';
+  var SELECTOR_QUEUE_TITLE = '#queue-name';
+  var SELECTOR_QUEUE_STATUS = '#queue-status';
+  var SELECTOR_QUEUE_PANEL = '#main-queue';
   var SELECTOR_TEMPLATE_QUEUE_ENTRY = '#template-queue-entry';
   var SELECTOR_NEWS_TICKER = '#news-ticker';
+  var QUEUE_INFO_3D_PRINTER = {title: "3D Printers",
+                              color: "#b60000"};
+  var QUEUE_INFO_DEFAULT = {title: "Default",
+                              color: "#283941"};
 
   var App = window.App;
   var Clock = App.Clock;
@@ -15,8 +22,15 @@
 
   var mainClock = new Clock(SELECTOR_CLOCK_WEEKDAY, SELECTOR_CLOCK_DATE, SELECTOR_CLOCK_TIME);
   var mainNewsTicker = new NewsTicker(SELECTOR_NEWS_TICKER);
-  var mainQueue = new Queue(SELECTOR_QUEUE, SELECTOR_TEMPLATE_QUEUE_ENTRY);
+  var mainQueue = new Queue(SELECTOR_QUEUE_HEADER, SELECTOR_QUEUE_TITLE, SELECTOR_QUEUE_STATUS, SELECTOR_QUEUE_PANEL, SELECTOR_TEMPLATE_QUEUE_ENTRY);
 
-  mainQueue.updateQueue([{position: 1, userName: "George Burdell"}, {position: 2, userName: "Sally Mae"}]);
+  mainQueue.updateQueue(QUEUE_INFO_3D_PRINTER.color, QUEUE_INFO_3D_PRINTER.title, "1 Available", [{position: 1, userName: "George Burdell"}, {position: 2, userName: "Sally Mae"}]);
+  setTimeout(function() {
+    mainQueue.updateQueue(QUEUE_INFO_DEFAULT.color, QUEUE_INFO_DEFAULT.title, "1 Available", [{position: 1, userName: "George Burdell"}, {position: 2, userName: "Sally Mae"}]);
+    setTimeout(function() {
+      mainQueue.updateQueue(QUEUE_INFO_3D_PRINTER.color, QUEUE_INFO_3D_PRINTER.title, "1 Available", [{position: 1, userName: "George Burdell"}, {position: 2, userName: "Sally Mae"}]);
+    }, 10000);
+  }, 5000);
+
 
 }) (window);
--- scripts/queue.js
+++ scripts/queue.js
@@ -4,61 +4,240 @@
   var App = window.App || {};
   var $ = window.jQuery;
 
-  function Queue(queueSelector, elementTemplateSelector) {
-    if (!queueSelector) {
+  function Queue(queueHeaderSelector, queueTitleSelector, queueStatusSelector, queuePanelSelector, elementTemplateSelector) {
+    //Ensure a proper selector for the queue header element is provided
+    if (!queueHeaderSelector) {
         throw new Error('No selector provided');
-    } else if (queueSelector.charAt(0) != '#') {
+    } else if (queueHeaderSelector.charAt(0) != '#') {
       throw new Error("Selector must be element id. Use # selector.");
     }
 
-    this.$queue = $(queueSelector);
-    if (this.$queue.length === 0) {
+    //Ensure the queue header element exists
+    this.$queueHeader = $(queueHeaderSelector);
+    if (this.$queueHeader.length === 0) {
       throw new Error('Could not find element with selector: ' + selector);
     }
 
+    //Ensure a proper selector for the queue title element is provided
+    if (!queueTitleSelector) {
+        throw new Error('No selector provided');
+    } else if (queueTitleSelector.charAt(0) != '#') {
+      throw new Error("Selector must be element id. Use # selector.");
+    }
+
+    //Ensure the queue title element exists
+    this.$queueTitle = $(queueTitleSelector);
+    if (this.$queueTitle.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
+
+    //Ensure a proper selector for the queue status element is provided
+    if (!queueStatusSelector) {
+        throw new Error('No selector provided');
+    } else if (queueStatusSelector.charAt(0) != '#') {
+      throw new Error("Selector must be element id. Use # selector.");
+    }
+
+    //Ensure the queue status element exists
+    this.$queueStatus = $(queueStatusSelector);
+    if (this.$queueStatus.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
+
+    //Ensure a proper selector for the queue panel element is provided
+    if (!queuePanelSelector) {
+        throw new Error('No selector provided');
+    } else if (queuePanelSelector.charAt(0) != '#') {
+      throw new Error("Selector must be element id. Use # selector.");
+    }
+
+    //Ensure the queue panel element exists
+    this.$queuePanel = $(queuePanelSelector);
+    if (this.$queuePanel.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
+
+    //Ensure a proper selector for the queue element template is provided
     if (!elementTemplateSelector) {
         throw new Error('No selector provided');
     } else if (elementTemplateSelector.charAt(0) != '#') {
       throw new Error("Selector must be element id. Use # selector.");
     }
 
+    //Ensure the queue element template exists
     if ($(elementTemplateSelector).length === 0) {
       throw new Error('Could not find element with selector: ' + selector);
     }
+    //Store the queue element template selector for later
     this.$elementTemplateSelector = elementTemplateSelector;
   };
 
+  //Display a new queue
+  Queue.prototype.updateQueue = function(queueColor, queueTitle, queueStatus, queueEntries) {
+    var headerBackgroundColor = queueColor;
+    var panelBackgroundColor = lightenColor(queueColor, 95);
+    var elementBackgroundColor = lightenColor(queueColor, 80);
+    var elementBorderColor = queueColor;
 
-  Queue.prototype.updateQueue = function(queue) {
-    this.$queue.find('.queue-entry').remove();
+    //Fade in the new header background color
+    //The fade effect comes from the transition property in queue_panel.css
+    this.$queueHeader.css({
+      "background-color": headerBackgroundColor
+    });
 
-    if (queue.length > 0) {
-      queue.sort(function (a, b) {
-        return a.position - b.position;
-      });
-      queue.forEach(function(entry) {
-        var $newEntry = new Entry(this.$elementTemplateSelector, entry).$element;
-        this.$queue.append($newEntry);
-      }.bind(this));
-    } else {
-      var $div = $('<div></div>', {
-        'class': 'queue-entry'
-      });
-      $div.append("No Queue");
-      this.$queue.append($div);
-    }
+    //Fade in the new panel background color
+    //The fade effect comes from the transition property in queue_panel.css
+    this.$queuePanel.css({
+      "background-color": panelBackgroundColor
+    });
+
+    //Fade in the new queue title
+    this.$queueTitle.fadeOut(function() {
+      $(this).text(queueTitle).fadeIn();
+    });
+
+    //Fade in the new queue status
+    this.updateQueueStatus(queueStatus);
+
+    //Fade in the new queue queue entries
+    this.updateQueueEntries(queueEntries, elementBackgroundColor, elementBorderColor);
   };
+
+  //Update the current queue status
+  Queue.prototype.updateQueueStatus = function(queueStatus) {
+    //Fade in the new queue status
+    this.$queueStatus.fadeOut(function() {
+      $(this).text(queueStatus).fadeIn();
+    });
+  }
+
+  //Update the current queue entries
+  Queue.prototype.updateQueueEntries = function(queueEntries, backgroundColor = null, borderColor = null) {
+    var queue = this; //Save the 'this' context, since it cannot be used in the promise function
+    //Find, fade out, and remove all of the existing queue entry elements
+    this.$queuePanel.find('.queue-entry').fadeOut().promise().done(function() {
+      //Remove the existing queue entry elements
+      this.remove();
+
+      //Then add the new elements
+      if (queueEntries.length > 0) { //If there are entries, create and display an element for each
+        queueEntries.sort(function (a, b) { //Sort the entires by queue position
+          return a.position - b.position;
+        });
+        queueEntries.forEach(function(entry) { //Create and display an element
+          //Create a new Entry and get the DOM element
+          var newEntry = new Entry(queue.$elementTemplateSelector, entry).$element;
+          if (backgroundColor && borderColor) {
+            newEntry.css({
+              "background-color": backgroundColor,
+              "border-color": borderColor
+            });
+          }
+          $(newEntry).hide(); //Hide the element
+          queue.$queuePanel.append(newEntry); //Add the element to the page
+          $(newEntry).fadeIn(); //Fade the element in
+        });
+      } else { //If there are no elements, create and display an element showing the queue is empty
+        //Create a new Entry that says "No Queue" and get the DOM element
+        var emptyEntry = new Entry(queue.elementTemplateSelector, {message: "No Queue"}).$element;
+        if (backgroundColor && borderColor) {
+          newEntry.css({
+            "background-color": backgroundColor,
+            "border-color": borderColor
+          });
+        }
+        $(newEntry).hide(); //Hide the element
+        queue.$queuePanel.append(newEntry); //Add the element to the page
+        $(newEntry).fadeIn(); //Fade the element in
+      }
+    });
+  }
 
   function Entry(templateSelector, queueEntry) {
-    //Get a copy of the template
-    var elementDiv = $(templateSelector)[0].content.cloneNode(true);
+    //Get the template's inner HTML, then create an element from it
+    //The syntax here is very picky. Other methods of creating elements
+    //from templates can result in "Document Fragments" which cannot hide or fade in
+    var elementDiv = $($(templateSelector).html());
+
+    //Get the title label within the element
     var titleContent = $(elementDiv).find('.queue-entry-title-content');
 
-    var description = queueEntry.position + ". " + queueEntry.userName;
+    //Ensure the title label element exists
+    if (titleContent.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
+
+    //Create the title lable text
+    var description = "";
+    //If the entry being created is for a user, add their queue position (number) and their name
+    if (queueEntry.position) {
+      description = queueEntry.position + ". " + queueEntry.userName;
+    } else { //If not, the queue is entry, so add the provided "No Queue" mssage
+      description = queueEntry.message;
+    }
+    //Add the text into the title label element
     titleContent.append(description);
 
+    //Set this $element property to the newly created element
+    //$element will be accessed by the caller
     this.$element = elementDiv;
   };
+
+  //Much of the logic in this function is copied from https://gist.github.com/mjackson/5311256
+  function lightenColor(hexColor, lightness) {
+    //Hex to RGB
+    var r = parseInt(hexColor.slice(1, 3), 16);
+    var g = parseInt(hexColor.slice(3, 5), 16);
+    var b = parseInt(hexColor.slice(5, 7), 16);
+
+    //RGB to HSL
+    r /= 255, g /= 255, b /= 255;
+
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if (max == min) {
+      h = s = 0; // achromatic
+    } else {
+      var d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+      switch (max) {
+        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+        case g: h = (b - r) / d + 2; break;
+        case b: h = (r - g) / d + 4; break;
+      }
+
+      h /= 6;
+    }
+
+    //Manually change the lightness
+    l = lightness / 100.0;
+
+    //HSL to RGB
+    if (s == 0) {
+      r = g = b = l; // achromatic
+    } else {
+      function hue2rgb(p, q, t) {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1/6) return p + (q - p) * 6 * t;
+        if (t < 1/2) return q;
+        if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+        return p;
+      }
+
+      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      var p = 2 * l - q;
+
+      r = hue2rgb(p, q, h + 1/3) * 255;
+      g = hue2rgb(p, q, h) * 255;
+      b = hue2rgb(p, q, h - 1/3) * 255;
+    }
+
+      var rgb = (r << 16) | (g << 8) | b;
+      return '#' + (0x1000000 + rgb).toString(16).slice(1)
+    }
 
   App.Queue = Queue;
   window.App = App;
