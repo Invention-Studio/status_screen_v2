f23480d73c9a80e698abd150d358f00d4ecb364d
Nick Rupert <nickrupert7@gmail.com>
2018-07-26 20:14:52 -0400

    Updated news_ticker as class

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-  prepareAndAnimateTickers();
-});
+(function (window) {
+  'use strict';
+
+  var SELECTOR_CLOCK_WEEKDAY = '#main-header-date-time-day';
+  var SELECTOR_CLOCK_DATE = '#main-header-date-time-date';
+  var SELECTOR_CLOCK_TIME = '#main-header-date-time-time';
+  var SELECTOR_NEWS_TICKER = '#news-ticker';
+
+  var App = window.App;
+  var NewsTicker = App.NewsTicker;
+
+  var mainNewsTicker = new NewsTicker(SELECTOR_NEWS_TICKER);
+
+}) (window);
--- scripts/news_ticker.js
+++ scripts/news_ticker.js
@@ -1,36 +1,81 @@
-function prepareAndAnimateTickers() {
-  $('.news-ticker').each(function() {
-    var news_ticker = this;
+(function (window) {
+  'use strict';
+
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  //Sets the scroll speed of the ticker
+  var TICKER_SPEED = 125; //px per second
+
+  function NewsTicker(tickerSelector) {
+    //Ensure a proper selector for the ticker element is provided
+    if (tickerSelector.charAt(0) != '#') {
+      throw new Error("Selector must be element id. Use # selector.");
+    }
+
+    //Get the ticker element
+    var news_ticker = $(tickerSelector)[0];
+
+    //Build the ticker message
     var message = "";
-    $('.news-ticker li span').each(function() {
+    $(news_ticker).find('li').each(function() { //Add the contents of each li to the message
       message += this.innerText + " ⦁ ";
     });
-    $('.news-ticker ul').each(function() {
+    $(news_ticker).find('ul').each(function() { ///Remove the original HTML list
       news_ticker.removeChild(this);
     });
+
+    //Create a table - a table is used to center the message text vertically
     var table = document.createElement("table");
     $(table).css({ 'width': '100%', 'height': '100%' });
     news_ticker.appendChild(table);
+
+    //Create a single row/column cell entry
     var tr = document.createElement("tr");
     table.appendChild(tr);
     var td = document.createElement("td");
     table.appendChild(td);
 
+    //Fill the table cell with the constructed message
     var span = document.createElement("span");
-    span.className = "ticker-span";
-    span.innerText = message + message;
+    span.className = "ticker-span"; //This class is used in news_ticker.css for styling
+    span.innerText = message + message; //Double the message up to allow for
+                                        //continuous scrolling without gaps
     td.appendChild(span);
 
-    var textWidth = span.clientWidth / 2;
-    var time = (textWidth / 125) * 1000;
-    animate(span, textWidth, time);
-  });
-}
+    //Ensure the message is long enough to have continuous scrolling without gaps
+    if (span.clientWidth / 2 < news_ticker.clientWidth) {
+      throw new Error("Text must be at least as wide as the container");
+    }
 
-function animate(element, distance, time) {
-  $(element).css({ 'right': '0' });
-  $(element).animate({ 'right': distance + 'px' },
-    time, 'linear', function() {
-      animate(element, distance, time);
-    });
-}
+    //Calculate the width of the message
+    //Divide by two to get the width of one copy of the message, since it is doubled up
+    var textWidth = span.clientWidth / 2;
+
+    //Calculate the scroll time for one copy of the message
+    var time = (textWidth / TICKER_SPEED) * 1000;
+
+    //Start the animation
+    animate.call(this, span, textWidth, time);
+  };
+
+  function animate(element, distance, time) {
+    //Set the left edge of the text flush with the left edge of the container
+    $(element).css({ 'left': '0' });
+
+    //Scroll the element to the left by half of it's width
+    //(i.e. the width of one copy of the message)
+    //When the element reaches the end of the animation,
+    //the second copy of the message will be flush with the left edge of the container,
+    //which will appear exactly the same as the beginning of the animation
+    //At this point, recursively call animate() to reset the animation and continue scrolling
+    $(element).animate({ 'left': -distance + 'px' },
+      time, 'linear', function() {
+        animate(element, distance, time);
+      });
+  };
+
+  App.NewsTicker = NewsTicker;
+  window.App = App;
+
+}) (window);
